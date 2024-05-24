4e3947776e8e6b1a0060bb72a0dc02645236c107
nathanbrn <breno_nathanael@hotmail.com>
2024-05-23 22:21:00 -0300

    update database

---
+++ data/database.ts
@@ -0,0 +1,17 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+export const db = {
+  users: [
+    {
+      id: "fsfs45fsd5f",
+      name: "Victoria",
+      email: "victoria.mail.com",
+      password: "password",
+    },
+  ],
+};
